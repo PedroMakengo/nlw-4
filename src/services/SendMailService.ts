9801a3caf0967b7e61c7641f3e942d3edc7d74b5
PedroMakengo <pedromakengomiguel@gmail.com>
2025-06-21 23:20:40 +0100

    Send mailer options

--- src/services/SendMailService.ts
+++ src/services/SendMailService.ts
@@ -1,5 +1,9 @@
 import nodemailer, { Transporter } from 'nodemailer'
 
+import handlebars from 'handlebars'
+import { resolve } from 'path'
+import fs from 'fs'
+
 class SendMailService {
   private client: Transporter
 
@@ -17,11 +21,17 @@ class SendMailService {
       this.client = transporter
     })
   }
-  async execute(to: string, subject: string, body: string) {
+  async execute(to: string, subject: string, variables: object, path: string) {
+    const templateFileContent = fs.readFileSync(path).toString('utf8')
+
+    const mailTemplateParse = handlebars.compile(templateFileContent)
+
+    const html = mailTemplateParse(variables)
+
     const message = await this.client.sendMail({
       to,
       subject,
-      html: body,
+      html,
       from: 'NPS <noreplay@nps.com.br>',
     })
 
