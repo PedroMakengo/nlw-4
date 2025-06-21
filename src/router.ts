4984174234c4760f57631bd4587393828c31e0cf
PedroMakengo <pedromakengomiguel@gmail.com>
2025-06-21 21:57:44 +0100

    Config sendmailer and styles text

--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express'
 import { UserController } from './controllers/UserController'
 import { SurveysController } from './controllers/SurveysController'
+import { SendMailController } from './controllers/SendMailController'
 
 const router = Router()
 
 const userController = new UserController()
 const surveyControler = new SurveysController()
+const sendMailController = new SendMailController()
 
 router.post('/users', userController.create)
 router.post('/surveys', surveyControler.create)
 router.get('/surveys', surveyControler.show)
+router.post('/sendMail', sendMailController.execute)
 
 export { router }
