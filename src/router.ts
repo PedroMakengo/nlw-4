4d4a9e557931ef31bb0daba432b3c39a87ad582d
PedroMakengo <pedromakengomiguel@gmail.com>
2025-06-30 06:02:20 +0100

    Adicionando a parte do answer

--- src/router.ts
+++ src/router.ts
@@ -2,16 +2,19 @@ import { Router } from 'express'
 import { UserController } from './controllers/UserController'
 import { SurveysController } from './controllers/SurveysController'
 import { SendMailController } from './controllers/SendMailController'
+import { AnswerController } from './controllers/AnswerController'
 
 const router = Router()
 
 const userController = new UserController()
 const surveyControler = new SurveysController()
 const sendMailController = new SendMailController()
+const answerController = new AnswerController()
 
 router.post('/users', userController.create)
 router.post('/surveys', surveyControler.create)
 router.get('/surveys', surveyControler.show)
 router.post('/sendMail', sendMailController.execute)
+router.post('/answer', answerController.execute)
 
 export { router }
